590064bd726d4ca7a641f61aff570418633d6c0d
zqm64 <zqm64@outlook.com>
2020-07-23 03:10:21 +0800

    ugg: Drop FingerprintExtensionService
    It doesn't work, and hard to fix

--- ugg-vendor-fingerprint.mk
+++ ugg-vendor-fingerprint.mk
@@ -22,6 +22,3 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/ugg/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib_fpc_tac_shared.so \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/hw/fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.default.so
-
-PRODUCT_PACKAGES += \
-    FingerprintExtensionService
